0b1e22da1de58cb8f372ff3e93a9049f34f3f96e
Christian Pardo <chrispardopersonal@gmail.com>
2020-04-01 19:40:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any? {|calls| calls  }
+  planeteer_calls.any? {|calls| call.lenght < 4 }
   
 end
 
